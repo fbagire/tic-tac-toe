98d1c828289d5c4c4cc7cec1cd2eb362c030143e
Faith Bagire <fbagire@drd.gov.rw>
2024-04-28 21:30:55 +0200

    player's turn and squencing

--- script.js
+++ script.js
@@ -15,11 +15,59 @@ function gameBoard() {
 
 function play() {
     let availCells = gameBoard().cell;
-    const player=gameBoard().player;
-    console.log(`Player ${player.starter? 'Two': 'One'}'s Turn` )
-    
-    // const starter= player.starter? 'playerTwo': 'playerOne'
-    prompt('choose your cell')
+    const player = gameBoard().player;
+    console.log(`Player ${player.starter ? 'Two' : 'One'}'s Turn`)
 
+    let starter = player.starter ? true : false
+    for (let index = 0; index < 9; index++) {
+
+        // prompt('choose your cell')
+        if (starter) {
+            let play2 = prompt("Player 2: Choose")
+            if (play2) {
+                availCells[play2] = !availCells[play2] ? 'O' : console.log('space used');
+                starter = !starter
+
+            }
+            else {
+                play2 = prompt("Player 2: Choose")
+
+            }
+        }
+        else {
+            let play1 = prompt("Player 1: Choose")
+            if (play1) {
+                availCells[play1] = !availCells[play1] ? 'X' : console.log('space used')
+                starter = !starter
+
+            }
+
+            else {
+                play1 = prompt("Player 1: Choose")
+
+            }
+        }
+    }
+    return {availCells}
+}
+
+
+function checkWinner(board){
+    // win
+
+
+
+    if (board['a1']==board['a2']==board['a3'])
+    if (board['a4']==board['a5']==board['a6'])
+    if (board['a7']==board['a8']==board['a9'])
+
+
+    if (board['a1']==board['a4']==board['a7'])
+    if (board['a2']==board['a5']==board['a8'])
+    if (board['a3']==board['a6']==board['a9'])
+    
+    if (board['a1']==board['a5']==board['a9'])
+    if (board['a3']==board['a5']==board['a7'])
+    
 
 }

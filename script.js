9a665845256a79974061eb8fe204ba74018c9a85
Faith Bagire <fbagire@drd.gov.rw>
2024-04-29 10:06:40 +0200

    check winner function prettify

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ function play() {
             let play2 = prompt("Player 2: Choose")
             if (play2) {
                 availCells[play2] = !availCells[play2] ? 'O' : console.log('space used');
+                checkWinner(availCells, player)
+
                 starter = !starter
 
             }
@@ -38,6 +40,7 @@ function play() {
             let play1 = prompt("Player 1: Choose")
             if (play1) {
                 availCells[play1] = !availCells[play1] ? 'X' : console.log('space used')
+                checkWinner(availCells)
                 starter = !starter
 
             }
@@ -53,38 +56,62 @@ function play() {
 
 
 function getPlayer(symbol) {
-    return symbol === player.playerOne ? 'Player One' : symbol === player.playerTwo ? 'Player Two' : 'Unknown Player';
+    return symbol === player.playerOne ? 'Player One' : symbol === player.playerTwo ? 'Player Two ' : 'Unknown Player';
 }
-function checkWinner(board, player) {
 
 
+function checkWinner(board) {
+    // Define winning conditions
+    const winningConditions = [
+        ['a1', 'a2', 'a3'],
+        ['a4', 'a5', 'a6'],
+        ['a7', 'a8', 'a9'],
+        ['a1', 'a4', 'a7'],
+        ['a2', 'a5', 'a8'],
+        ['a3', 'a6', 'a9'],
+        ['a1', 'a5', 'a9'],
+        ['a3', 'a5', 'a7']
+    ];
 
-
-
-    if (board['a1'] == board['a2'] == board['a3']) {
-        return getPlayer(board['a1'])
+    // Check each winning condition
+    for (const condition of winningConditions) {
+        const [cell1, cell2, cell3] = condition;
+        if (board[cell1] && board[cell1] === board[cell2] && board[cell2] === board[cell3]) {
+            console.log(getPlayer(board[cell1]) + ' wins!');
+            return;
+        }
     }
-    else if (board['a4'] == board['a5'] == board['a6']) {
-        return getPlayer(board['a1'])
-    }
-    else if (board['a7'] == board['a8'] == board['a9']) {
-        return getPlayer(board['a7'])
-    }
-    else if (board['a1'] == board['a4'] == board['a7']) {
-        return getPlayer(board['a1'])
-    }
-    else if (board['a2'] == board['a5'] == board['a8']) {
-        return getPlayer(board['a2'])
-    }
-    else if (board['a3'] == board['a6'] == board['a9']) {
-        return getPlayer(board['a3'])
-    }
-    else if (board['a1'] == board['a5'] == board['a9']) {
-       return getPlayer(board['a1'])
-    }
-    else if (board['a3'] == board['a5'] == board['a7']) {
-        return getPlayer(board['a3'])
-    }
-
-
 }
+
+
+
+
+// function checkWinner(board) {
+
+//     if (board['a1'] === board['a2'] === board['a3']) {
+//         console.log(getPlayer(board['a1']))
+//     }
+//     else if (board['a4'] === board['a5'] === board['a6']) {
+//         console.log(getPlayer(board['a1']))
+//     }
+//     else if (board['a7'] === board['a8'] === board['a9']) {
+//         console.log(getPlayer(board['a7']))
+//     }
+//     else if (board['a1'] === board['a4'] === board['a7']) {
+//         console.log(getPlayer(board['a1']))
+//     }
+//     else if (board['a2'] === board['a5'] === board['a8']) {
+//         console.log(getPlayer(board['a2']))
+//     }
+//     else if (board['a3'] === board['a6'] === board['a9']) {
+//         console.log(getPlayer(board['a3']))
+//     }
+//     else if (board['a1'] === board['a5'] === board['a9']) {
+//         console.log(getPlayer(board['a1']))
+//     }
+//     else if (board['a3'] === board['a5'] === board['a7']) {
+//         console.log(getPlayer(board['a3']))
+//     }
+
+
+// }
